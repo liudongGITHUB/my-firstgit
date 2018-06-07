8c116692f093764b3bc1f6cc74e311928b1c8991
liudong <154917034@qq.com>
2018-06-07 21:30:13 +0800

    first commit

---
+++ src/com/ldd/test/Demo.java
@@ -0,0 +1,11 @@
+package com.ldd.test;
+
+public class Demo {
+    public static void main(String[] args){
+        System.out.print("老婆你好1");
+        System.out.print("老婆你好2");
+        System.out.print("老婆你好3");
+        System.out.print("老婆你好4");
+        System.out.print("老婆你好5");
+    }
+}
